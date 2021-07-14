f28a6e7b4de8cf27d24e8e444250dab8f868168c
jobymulamoottilsoftware@gmail.com <jobymulamoottilsoftware@gmail.com>
2021-07-14 15:14:48 +0530

    Implemented New Theme for Project and also changed Login and Register Page, and included required css and js files.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Leave_Management.Models;
+﻿using AutoMapper;
+using Leave_Management.Contracts;
+using Leave_Management.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,18 +12,35 @@ using System.Threading.Tasks;
 
 namespace Leave_Management.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ILeaveTypeRepository _leavetyperepos;
+        private readonly ILeaveRequestRepository _leaveRequestRepo;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ILeaveTypeRepository leavetyperepos, 
+                              IMapper mapper, ILeaveRequestRepository leaveRequestRepo)
         {
             _logger = logger;
+            _leavetyperepos = leavetyperepos;
+            _leaveRequestRepo = leaveRequestRepo;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var leaveRequests = _leaveRequestRepo.FindAll();
+            var leaveRequestModel = _mapper.Map<List<LeaveRequestVM>>(leaveRequests);
+            var model = new HomeVM
+            {
+                TotalRequests = leaveRequestModel.Count,
+                ApprovedRequests = leaveRequestModel.Count(q => q.Approved == true),
+                PendingRequests = leaveRequestModel.Count(q => q.Approved == null),
+                RejectedRequests = leaveRequestModel.Count(q => q.Approved == false),
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Models/HomeVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Models
+{
+    public class HomeVM
+    {
+        [Display(Name = "Total Requests")]
+        public int TotalRequests { get; set; }
+
+        [Display(Name = "Approved Requests")]
+        public int ApprovedRequests { get; set; }
+
+        [Display(Name = "Pending Requests")]
+        public int PendingRequests { get; set; }
+
+        [Display(Name = "Rejected Requests")]
+        public int RejectedRequests { get; set; }
+    }
+}
