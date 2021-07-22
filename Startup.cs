ed2f58030343c4e0f37b3b8419064333db19ff00
jobymulamoottilsoftware@gmail.com <jobymulamoottilsoftware@gmail.com>
2021-07-22 20:00:55 +0530

    Small Changes

--- Startup.cs
+++ Startup.cs
@@ -32,9 +32,20 @@ namespace Leave_Management
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("AzureConnection")));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            }
+
+            services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
 
             //Add References for Contracts and Repository to startup file
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
